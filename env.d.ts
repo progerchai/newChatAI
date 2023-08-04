c7a852c7fa864f1fe3cdcff90e5ea433a34e2e41
proger <progerchai@qq.com>
2023-08-04 23:54:31 +0800

    upgrade vue2 to vue3

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+declare module 'lodash';
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
