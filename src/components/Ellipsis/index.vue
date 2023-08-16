9fed6f78fff62ca825ebaf1678efd8fb3e4f1490
proger <progerchai@qq.com>
2023-08-17 00:43:01 +0800

    ts lint

--- src/components/Ellipsis/index.vue
+++ src/components/Ellipsis/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import _ from 'lodash';
 withDefaults(defineProps<{ text: string | undefined; max?: number }>(), {
   max: 8,
 });
 </script>
 <template>
   <span>
-    {{ text.length > max ? text.slice(0, max) + '...' : text }}
+    {{ _.get(text, 'length', 0) > max ? text?.slice(0, max) + '...' : text }}
   </span>
 </template>
