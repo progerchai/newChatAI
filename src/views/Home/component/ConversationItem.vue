c5f0cebcbc254c37799f51c7aa36f4e9f52578b8
proger <progerchai@qq.com>
2023-08-19 02:05:36 +0800

    fix: markdown render null in prod env

--- src/views/Home/component/ConversationItem.vue
+++ src/views/Home/component/ConversationItem.vue
@@ -1,53 +1,15 @@
 <script setup lang="ts">
 import { IconBad, IconGood } from '@/components/icons';
 import type { IConversation } from '@/types';
-import hljs from 'highlight.js';
 import Markdown from 'vue3-markdown-it';
 import 'highlight.js/styles/monokai.css';
 import _ from 'lodash';
-import * as mathjaxPlugin from 'markdown-it-mathjax3';
+import mathjaxPlugin from 'markdown-it-mathjax3';
 
-const katexStr = '$$\Gamma(z) = \int_0^\infty t^{z-1}e^{-t}dt\,.$$';
-const table =
-  '|  表头   | 表头  |\n|  ----  | ----  |\n| 单元格  | 单元格 |\n| 单元格  | 单元格 |';
 const props = defineProps<{
   conv: IConversation;
   idx: number | undefined;
 }>();
-const renderer = {
-  code(code: string, infostring: string, escaped: boolean) {
-    console.log(22222, code, infostring, escaped);
-
-    var codeHtml = code;
-    if (infostring && infostring == 'html') {
-      codeHtml = encodeURIComponent(code);
-    }
-    if (infostring) {
-      codeHtml = hljs.highlightAuto(code).value;
-    }
-
-    console.log(code, infostring, escaped, codeHtml);
-
-    return `<div class="bg-black mb-4 rounded-md">
-      <div class="code_header flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans">
-        <span>${infostring || ''}</span>
-        <button onclick="copy(this)" class="flex ml-auto gap-2">
-          <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
-          </svg>
-          <span>Copy code</span>
-          <code style="display:none">${encodeURIComponent(code)}</code>
-        </button>
-      </div>
-      <div class="p-4 overflow-y-auto">
-        <code class="!whitespace-pre hljs language-${infostring}">${codeHtml}</code>
-      </div>
-    </div>`;
-  },
-  paragraph(text: string) {
-    return `<p style="white-space:pre-wrap;">${text}</p>`;
-  },
-};
 
 function suitable(idx: number | undefined, conv: IConversation, suit: any) {}
 </script>
