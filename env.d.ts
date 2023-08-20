2aa9a275d8a32d3165e332bf3a4960b6b75e4de7
proger <progerchai@qq.com>
2023-08-20 18:41:18 +0800

    chore: vuex declare

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,7 @@ declare module 'highlight.js';
 declare module 'vue3-markdown-it';
 declare module 'element-plus';
 declare module 'markdown-it-mathjax3';
+declare module 'vuex';
 
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
